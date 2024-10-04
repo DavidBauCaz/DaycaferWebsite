e56a0b50b085c9f35110e80a7bee96393803a72e
DavidBauCaz <DavidBauCaz@gmail.com>
2024-10-04 15:01:05 -0600

    Correcciones en modelo y se agregaron las funciones de los controladores

--- core/views.py
+++ core/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from models import Usuario # importamos al usuario definido en core/models.py
 
 # Create your views here.
+def user_controller():
+    
+    pass
+
+def game_page_controller():
+    pass
+
+def forum_controller():
+    pass
